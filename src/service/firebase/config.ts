38398ce853e52a328cd32223c83cc01c42e88e77
Oded Goldglas <oded.godglas@fiverr.com>
2024-04-27 21:55:29 +0300

    Formatting Cleanups Firebase starter

---
+++ src/service/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCceWnYrbo2ilDAHH1axXt41yyWRW8XpNk',
+  authDomain: 'portfolio-tracker-73341.firebaseapp.com',
+  projectId: 'portfolio-tracker-73341',
+  storageBucket: 'portfolio-tracker-73341.appspot.com',
+  messagingSenderId: '536377237793',
+  appId: '1:536377237793:web:ca17aa5991de1df3205e60',
+};
