9d6990a274f3cd5fad263199719d5fd89bf0d5f2
Oded Goldglas <oded.godglas@fiverr.com>
2024-04-28 11:32:16 +0300

    .

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -1,6 +1,9 @@
 import { boot } from 'quasar/wrappers';
 import { initializeFirebaseApp } from '../service/firebase';
 
+/**
+ * Initialize Firebase application end.
+ */
 export default boot(() => {
   initializeFirebaseApp();
 });
