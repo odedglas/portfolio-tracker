75525948235547a381c35b2bb7192aebf69c015c
odedglas <odedglas@gmail.com>
2024-04-28 14:03:11 +0300

    Fix Firebase deployment CI (#7)

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -1,6 +1,9 @@
 import { boot } from 'quasar/wrappers';
 import { initializeFirebaseApp } from '../service/firebase';
 
+/**
+ * Initialize Firebase application.
+ */
 export default boot(() => {
   initializeFirebaseApp();
 });
