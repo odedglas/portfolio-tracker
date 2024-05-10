9d0358a86d9bf3c6198ada5666b1cdc4b22b9a5e
odedglas <odedglas@gmail.com>
2024-05-10 22:49:01 +0300

    Header navigation portfolio dropdown nav.

--- src/components/header/Navigation.vue
+++ src/components/header/Navigation.vue
@@ -1,37 +1,94 @@
 <template>
   <div class="navigation-buttons-container flex q-gutter-sm">
-    <q-btn
+    <component
       v-for="button in navigationButtons"
       :key="button.id"
+      :is="button.component"
       :class="navigationButtonClass(button.id)"
-      @click="() => navigate(button.route)"
+      @click="() => button.route && navigate(button.route)"
+      v-bind="button.props"
       flat
       no-caps
       color="grey-4"
     >
-      {{ button.text }}
-    </q-btn>
+      <q-menu v-if="button.menuOptions">
+        <q-item
+          v-for="option in button.menuOptions"
+          :key="option.id"
+          clickable
+          v-close-popup
+          @click="() => navigate(option.route)"
+        >
+          <q-item-section side>
+            <q-icon :name="option.icon" />
+          </q-item-section>
+          <q-item-section>{{ option.text }}</q-item-section>
+        </q-item>
+      </q-menu>
+    </component>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useRouter } from 'vue-router';
+import { QBtn, QBtnDropdown, QMenu } from 'quasar';
 
 export default defineComponent({
   name: 'AppNavigation',
+  components: {
+    QBtn,
+    QBtnDropdown,
+    QMenu,
+  },
   setup() {
     const router = useRouter();
 
     const navigationButtons = [
-      { id: 'dashboard', text: 'Dashboard', route: '/' },
+      {
+        id: 'dashboard',
+        route: '/',
+        component: 'q-btn',
+        props: { label: 'Dashboard' },
+      },
       {
         id: 'manage-portfolio',
-        text: 'Portfolio',
-        route: '/manage-portfolios',
+        props: { label: 'Portfolio' },
+        component: 'q-btn',
+        menuOptions: [
+          {
+            id: 'transactions',
+            text: 'Transactions',
+            icon: 'transform',
+            route: '/transactions',
+          },
+          {
+            id: 'holdings',
+            text: 'Holdings',
+            icon: 'cases',
+            route: '/holdings',
+          },
+          {
+            id: 'categories',
+            text: 'Categories',
+            icon: 'category',
+            route: '/categories',
+          },
+          { id: 'events', text: 'Events', icon: 'event', route: '/events' },
+        ],
       },
-      { id: 'analytics', text: 'Analytics', route: '/' },
-      { id: 'stock-plants', text: 'Stock Plans', route: '/' },
+      {
+        id: 'analytics',
+        route: '/',
+        component: 'q-btn',
+        props: { label: 'Analytics' },
+      },
+      {
+        id: 'stock-plants',
+        route: '/',
+        component: 'q-btn',
+        props: { label: 'Stocks Plans' },
+      },
     ];
 
     const isActiveRoute = (id: string) =>
