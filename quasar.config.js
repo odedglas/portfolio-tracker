c65b87842ddb1705385cd23f4714e2501e5f6131
odedgo <odedgo@monday.com>
2024-06-24 23:01:02 +0300

    Jesus

--- quasar.config.js
+++ quasar.config.js
@@ -54,8 +54,9 @@ module.exports = configure(function (/* ctx */) {
       target: {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node20',
-        env,
       },
+
+      env,
 
       vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
